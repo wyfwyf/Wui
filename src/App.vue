93bd36a2e47d96ed9124e74b1142cd492d96f625
王云飞 <wangyunfei@choobest.com>
2020-12-07 14:19:36 +0800

    【WUI】初始化

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <layout-admin>
+    <div>
+      console.log()
+    </div>
+  </layout-admin>
+</template>
+
+<script>
+import LayoutAdmin from '@/components/layout/admin'
+
+export default {
+  name: 'App',
+  components: {
+    LayoutAdmin
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/layout/admin/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class = "flex body">
+    <div class = "flex-item menu">
+      <ul>
+        <li>display:flex</li>
+      </ul>
+    </div>
+    <div class = "flex-item-a main">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LayoutAdmin'
+}
+</script>
+
+<style scoped>
+.body {
+  height: 100%;
+}
+
+.menu {
+  width: 200px;
+  padding: 20px;
+  background: #3c3c3c;
+  color: #eaeaea
+}
+
+.main {
+  background: #f1f1f1;
+  padding: 20px;
+}
+</style>
