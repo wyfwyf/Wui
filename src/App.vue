78825c21ec46c645318eb0a381d662088e198db5
王云飞 <962774231@qq.com>
2020-12-20 22:33:16 +0800

    【前端】【代码块】【测试】

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,16 @@
   <layout-admin>
     <div>
       console.log()
+      <codemirror
+        ref="myCm"
+        :value="code"
+        :options="cmOptions"
+        @ready="onCmReady"
+        @focus="onCmFocus"
+
+      >
+        @input="onCmCodeChange"
+      </codemirror>
     </div>
   </layout-admin>
 </template>
@@ -13,10 +23,42 @@ export default {
   name: 'App',
   components: {
     LayoutAdmin
+    // codemirror
+  },
+  data () {
+    return {
+      code: 'const a = 10',
+      cmOptions: {
+        // codemirror options
+        tabSize: 2,
+        mode: 'text/javascript',
+        theme: 'darcula',
+        lineNumbers: true,
+        line: true
+        // more codemirror options, 更多 codemirror 的高级配置...
+      }
+    }
+  },
+  methods: {
+    onCmReady(cm) {
+      console.log('the editor is readied!', cm)
+    },
+    onCmFocus(cm) {
+      console.log('the editor is focus!', cm)
+    },
+    onCmCodeChange(newCode) {
+      console.log('this is new code', newCode)
+      this.code = newCode
+    }
+  },
+  computed: {
+    codemirror() {
+      return this.$refs.myCm.codemirror
+    }
+  },
+  mounted() {
+    console.log('this is current codemirror object', this.codemirror)
+    // you can use this.codemirror to do something...
   }
 }
 </script>
-
-<style scoped>
-
-</style>
--- src/main.js
+++ src/main.js
@@ -3,11 +3,22 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 
+import VueCodemirror from 'vue-codemirror'
+
+// require styles
 import './assets/css/base.css'
 import 'element-ui/lib/theme-chalk/index.css'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/vue/vue'
+import 'codemirror/mode/css/css'
+import 'codemirror/mode/htmlmixed/htmlmixed'
+import 'codemirror/theme/darcula.css'
+
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+Vue.use(VueCodemirror)
 
 /* eslint-disable no-new */
 new Vue({
